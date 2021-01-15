cdfbc80065cd2e58b9960ec81a9d11bdef129d64
John Nicholas <john.nicholas@madetech.com>
2021-01-15 13:43:59 +0000

    starting point

---
+++ GR/ApprovalTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using csharpcore;
+using UT;
+using Xunit;
+
+namespace GR
+{
+    [UseReporter(typeof(DiffReporter))]
+    public class ApprovalTest
+    {
+        [Fact]
+        public void ThirtyDays()
+        {
+            var fakeoutput = new StringBuilder();
+            Console.SetOut(new StringWriter(fakeoutput));
+            Console.SetIn(new StringReader("a\n"));
+
+            Run();
+            var output = fakeoutput.ToString();
+            
+       //     File.WriteAllText("ApprovalTest.ThirtyDays.approved.txt", output);
+
+            Approvals.Verify(output);
+        }
+        
+        private void Run()
+        {
+            Console.WriteLine("OMGHAI!");
+
+            IList<Item> Items = new List<Item>{
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 15,
+                    Quality = 20
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 10,
+                    Quality = 49
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 5,
+                    Quality = 49
+                },
+                // this conjured item does not work properly yet
+                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+            };
+
+            var app = new GildedRose(Items);
+
+
+            for (var i = 0; i < 31; i++)
+            {
+                Console.WriteLine("-------- day " + i + " --------");
+                Console.WriteLine("name, sellIn, quality");
+                for (var j = 0; j < Items.Count; j++)
+                {
+                    System.Console.WriteLine(Items[j].Name + ", " + Items[j].SellIn + ", " + Items[j].Quality);
+                }
+                Console.WriteLine("");
+                app.UpdateQuality();
+            }
+        }
+    }
+    
+    
+    
+}
---
+++ GR/GildedRoseTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using csharpcore;
+using Xunit;
+
+namespace GR
+{
+    public class GildedRoseTest
+    {
+        [Fact]
+        public void foo()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal("fixme", Items[0].Name);
+        }
+
+
+     
+    }
+}
