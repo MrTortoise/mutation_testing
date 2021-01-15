810926248a3db1fb5e48cdfe77aebf1a20662a53
John Nicholas <john.nicholas@madetech.com>
2021-01-15 16:16:27 +0000

    congrats we got coverage

--- GR/ApprovalTest.cs
+++ GR/ApprovalTest.cs
@@ -34,30 +34,28 @@ namespace GR
 
             IList<Item> Items = new List<Item>{
                 new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 15,
-                    Quality = 20
-                },
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 10,
-                    Quality = 49
-                },
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 5,
-                    Quality = 49
-                },
-                // this conjured item does not work properly yet
-                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                new Item {Name = "", SellIn = 10, Quality = 20},
+                new Item {Name = "", SellIn = 11, Quality = 20},
+                new Item {Name = "Aged Brie"},
+                // new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 50},
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 49},
+                
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 11, Quality = 49},
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 49},
+                
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 49},
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 6, Quality = 49},
+                
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 12, Quality = 48},
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 11, Quality = 48},
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 48},
+                
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 48},
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 6, Quality = 48},
+                
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 6, Quality = 47},
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 47},
             };
 
             var app = new GildedRose(Items);
