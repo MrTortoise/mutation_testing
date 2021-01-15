445d103031d19583fcc3b15d9e4d317b726a280d
John Nicholas <john.nicholas@madetech.com>
2021-01-15 16:37:01 +0000

    put function inside if and then inlined it - should be lots of uncovered copde we can just delete now

--- UT/GildedRose.cs
+++ UT/GildedRose.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
+using csharpcore;
 
-namespace csharpcore
+namespace UT
 {
     public class GildedRose
     {
@@ -13,6 +14,14 @@ namespace csharpcore
         public void UpdateQuality()
         {
             for (var i = 0; i < Items.Count; i++)
+            {
+                Rewrite(i);
+            }
+        }
+
+        private void Rewrite(int i)
+        {
+            if (Items[i].Name == "Aged Brie")
             {
                 if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
@@ -81,6 +90,83 @@ namespace csharpcore
                         {
                             Items[i].Quality = Items[i].Quality + 1;
                         }
+                    }
+                }
+            }
+            else
+            {
+                TheActualUpdate(i); 
+            }
+        }
+
+        private void TheActualUpdate(int i)
+        {
+            if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (Items[i].Quality > 0)
+                {
+                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                    {
+                        Items[i].Quality = Items[i].Quality - 1;
+                    }
+                }
+            }
+            else
+            {
+                if (Items[i].Quality < 50)
+                {
+                    Items[i].Quality = Items[i].Quality + 1;
+
+                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (Items[i].SellIn < 11)
+                        {
+                            if (Items[i].Quality < 50)
+                            {
+                                Items[i].Quality = Items[i].Quality + 1;
+                            }
+                        }
+
+                        if (Items[i].SellIn < 6)
+                        {
+                            if (Items[i].Quality < 50)
+                            {
+                                Items[i].Quality = Items[i].Quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+            {
+                Items[i].SellIn = Items[i].SellIn - 1;
+            }
+
+            if (Items[i].SellIn < 0)
+            {
+                if (Items[i].Name != "Aged Brie")
+                {
+                    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (Items[i].Quality > 0)
+                        {
+                            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                            {
+                                Items[i].Quality = Items[i].Quality - 1;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                    }
+                }
+                else
+                {
+                    if (Items[i].Quality < 50)
+                    {
+                        Items[i].Quality = Items[i].Quality + 1;
                     }
                 }
             }
